96ccc7095bc794d320e5b6e007cd546851120a2b
Diego Alfonso Ballesteros Villamizar <diegob@student.ethz.ch>
2015-10-03 14:19:13 +0200

    For SSL verification

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import requests
 from persistent_helpers import get_recipe_info, get_recipe_short_descriptions
 from parse_helper import route_command
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
