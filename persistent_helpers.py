0903ec80c84fa765e5abf3d155030b00721b360f
Prashanth Kanduri <prashanth.kanduri@gmail.com>
2015-10-03 03:39:24 +0200

    Added a new function body

--- persistent_helpers.py
+++ persistent_helpers.py
@@ -1,6 +1,10 @@
 #!flask/bin/python
 import json
+import glob
+import os
+
 from flask import Flask, jsonify
+from creds.py import bigOvenAPIkey
 
 recipe_blob_dct = {
     1 : {
@@ -29,7 +33,26 @@ def get_recipe_ids():
 
     @return: a JSON object - list of recipe IDs, where recipe ID is a number
     """
-    return jsonify({'list': recipe_blob_dct.keys()})
+    """
+    1. Get listing of files in the directory
+    2. Make array of integers with the names of the files
+    """
+
+    #jsonCounter = len(glob.glob1(".../recipeJSONs/","*.json"))
+
+    #list = [None] * jsonCounter
+
+    dirListing = os.listdir(".../recipeJSONs/")
+    lst = []
+    tempName = []
+
+    for item in dirListing:
+        if ".json" in item:
+            tempName = os.path.splitext(item)[0]
+            lst.append(int(tempName))
+
+    return lst
+    #return jsonify({'list': recipe_blob_dct.keys()})
 
 
 def get_recipe_info(recipe_id):
@@ -38,7 +61,28 @@ def get_recipe_info(recipe_id):
     @type recipe_id: number
     @return: a JSON object
     """
+
+    """
+    1. Get list of files
+    2. If recipe_id present in list, get the contents of the files
+    3. Else, make API call to get the JSON data from bigOven
+    4. jsonify the data and return
+    """
+    list = get_recipe_ids()
+    if recipe_id in list:
+
+
+    """
     if recipe_id not in recipe_blob_dct.keys():
         return None
     else:
         return jsonify(recipe_blob_dct[recipe_id])
+    """
+
+
+def main():
+    print get_recipe_ids()
+
+
+if __name__ == '__main__':
+    main()
